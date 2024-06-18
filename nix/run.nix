a9d211a130272a00ff81345f388f02774c7a25ac
Infinidoge <infinidoge@inx.moe>
2024-06-18 10:24:08 -0400

    nix: fix run scripts

--- nix/run.nix
+++ nix/run.nix
@@ -3,35 +3,35 @@
 , pkgs-i386
 }:
 let
-  inherit (pkgs) writeScriptBin lib;
+  inherit (pkgs) writeShellScriptBin lib;
 
   tmp = "tmp=$(mktemp -d --tmpdir xenia-i386-kernel.XXXX); cd $tmp";
   i386 = "${pkgs.qemu}/bin/qemu-system-i386";
   qemuArgs = "-boot order=a -drive file=os-image.bin,index=0,if=floppy,format=raw -smp 4";
 in
 lib.mapAttrs (name: value: { type = "app"; program = lib.getExe value; }) {
-  vga = writeScriptBin "vga" ''
+  vga = writeShellScriptBin "vga" ''
     ${tmp}
     cp ${drv}/os-image.bin .
     chmod u+rwx os-image.bin
     ${i386} -vga std ${qemuArgs}
   '';
 
-  curses = writeScriptBin "curses" ''
+  curses = writeShellScriptBin "curses" ''
     ${tmp}
     cp ${drv}/os-image.bin .
     chmod u+rwx os-image.bin
     ${i386} -display curses ${qemuArgs}
   '';
 
-  debug = writeScriptBin "debug" ''
+  debug = writeShellScriptBin "debug" ''
     ${tmp}
     cp ${drv}/os-image.bin ${drv}/kernel.elf .
     chmod u+rwx os-image.bin
     ${i386} -display curses -s ${qemuArgs}
   '';
 
-  gdb = writeScriptBin "gdb" ''
+  gdb = writeShellScriptBin "gdb" ''
     ${tmp}
     cp ${drv}/kernel.elf .
     cp -r ${drv.src}/* .
